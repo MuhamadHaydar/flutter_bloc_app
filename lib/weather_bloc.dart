b9cd77ee119f4f6a09c69446817a2c8bc42a20bf
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-09-20 13:46:50 +0300

    Change bloc class to work with events and states.

--- lib/weather_bloc.dart
+++ lib/weather_bloc.dart
@@ -3,17 +3,39 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc_app/data/model/weather.dart';
+import 'package:flutter_bloc_app/data/weather_repository.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial());
+  WeatherRepository weatherRepository;
+
+  WeatherBloc(this.weatherRepository);
+
+  @override
+  WeatherState get initialState => WeatherInitial();
 
   @override
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield WeatherLoading();
+    if (event is GetWeather) {
+      try {
+        final weather = await weatherRepository.fetchWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on WeatherError {
+        yield WeatherError("Couldn't fetch weather. Is the device online?");
+      }
+    } else if (event is GetWeatherDetail) {
+      try {
+        final weather =
+            await weatherRepository.fetchDetailedWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on WeatherError {
+        yield WeatherError("Couldn't fetch weather. Is the device online?");
+      }
+    }
   }
 }
