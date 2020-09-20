78fae46602485fad3d315551b6760d5294faf1d9
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-09-20 12:01:07 +0300

    Add weather events.

--- lib/weather_event.dart
+++ lib/weather_event.dart
@@ -3,3 +3,21 @@ part of 'weather_bloc.dart';
 abstract class WeatherEvent extends Equatable {
   const WeatherEvent();
 }
+
+class GetWeather extends WeatherEvent {
+  final String cityName;
+
+  const GetWeather({this.cityName});
+
+  @override
+  List<Object> get props => [cityName];
+}
+
+class GetWeatherDetail extends WeatherEvent {
+  final String cityName;
+
+  const GetWeatherDetail({this.cityName});
+
+  @override
+  List<Object> get props => [cityName];
+}
