e3bf8055c33e0d556471ec3bfc1b5aadd97c4000
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-09-22 08:50:38 +0300

    Provide bloc from main class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app/data/weather_repository.dart';
+import 'package:flutter_bloc_app/weather_bloc.dart';
 
 import 'pages/weather_search_page.dart';
 
@@ -8,8 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather App',
-      home: WeatherSearchPage(),
+        title: 'Weather App',
+        home: BlocProvider(
+          builder: (context) => WeatherBloc(FakeWeatherRepository()),
+          child: WeatherSearchPage(),
+        )
     );
   }
 }
